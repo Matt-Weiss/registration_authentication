90c6ffea0a443eba9844918baa64c78f030bfd39
Matt Weiss <weiss.matt@outlook.com>
2019-03-28 15:01:41 -0600

    new user non-duplication test failing and I can't figure out why

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    if User.find(user_params[:email])
+    if User.find_by(email: user_params[:email])
       flash.now[:error] = "User with that email already exists"
       redirect_to new_user_path
     else
       @user = User.new(user_params)
-      if User.save
+      if @user.save
         flash.now[:notice] = "Thanks for registering!"
         redirect_to users_path
       else
@@ -24,7 +28,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require[:user].permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
--- spec/features/new_user_spec.rb
+++ spec/features/new_user_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe 'When a visitor creates a new account' do
     expect(page).to have_content("Email:")
     expect(page).to have_content("Password:")
 
-    fill_in 'user[name]' with: 'Matt Weiss'
-    fill_in 'user[email]' with: 'generic@email.com' 
-    fill_in 'user[password]' with: 'generic_password'
-    click_on 'Submit'
+    fill_in 'user[name]', with: 'Matt Weiss2'
+    fill_in 'user[email]', with: 'generic2@email.com'
+    fill_in 'user[password]', with: 'generic_password2'
+    click_on 'Create User'
     expect(current_path).to eq(users_path)
 
   end
